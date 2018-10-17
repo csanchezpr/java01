220eb284031231c7ae1b68f601fc80a3aef7e842
Carlos <Carlos@DESKTOP-0G6LC4N>
2018-10-17 18:01:07 +0200

    Versió 0.2 del projecte java01

--- prjava01/src/prjava01/Prjava01.java
+++ prjava01/src/prjava01/Prjava01.java
@@ -9,16 +9,20 @@ package prjava01;
  *
  * @author csanchezpr
  */
+
 import java.io.*;
 
 public class Prjava01 {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        
         File f = new File("fitxer.html");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("     	<h1>DAW2 m08uf8pr2</h1>");bw.newLine();
             bw.write("<html>");
             bw.newLine();
             bw.write("  <head>");
